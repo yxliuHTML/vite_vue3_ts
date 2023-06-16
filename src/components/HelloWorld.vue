a9f491c6d868ed9fa1c1c423142225978a391809
yxliu <79382207@qq.com>
2023-06-16 15:45:13 +0800

    fix: 修复bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,8 +35,12 @@ const add = () => {
 	<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .read-the-docs {
 	color: #888;
+
+	.box {
+		color: #000;
+	}
 }
 </style>
